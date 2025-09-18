fa1010755d3803f53a7713b32a0459fdc2ebe7a4
Virginie <143293036+Virginouille@users.noreply.github.com>
2025-09-18 10:12:28 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Fragment } from "react"
+
+const title = "Bonjour vous"
+
+function App() {
+  return <>
+
+    <h1 id="title" className="title"> {title}</h1>
+    <input type="text" />
+
+    <p>bla blablrferfergfregergrgergfre</p>
+  </>
+}
+
+export default App
